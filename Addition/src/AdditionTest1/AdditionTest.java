fd3cf76fa32586edec46e78f706b3a42acaa6036
spselenim <pawal.satish@gmail.com>
2020-09-06 15:47:58 +0530

    First

---
+++ Addition/src/AdditionTest1/AdditionTest.java
@@ -0,0 +1,12 @@
+package AdditionTest1;
+
+public class AdditionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int Num1=19;
+int Num2=2;
+System.out.println("Addition of 2 Numbers" + Num1+Num2);
+	}
+
+}
